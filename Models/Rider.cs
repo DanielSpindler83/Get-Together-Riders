d840a06f04c7a56c3adc30d29163175918ed6ffd
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2023-05-02 21:21:57 +1000

    Add Rider class

---
+++ Models/Rider.cs
@@ -0,0 +1,20 @@
+﻿namespace Get_Together_Riders.Models
+{
+    public class Rider
+    {
+        public int RiderID { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string DisplayName { get; set; } = string.Empty;
+        public string? Bio { get; set; }
+        public string? Email { get; set; } // should be set same as facebook email login when this is added
+        public string? PhoneNumber { get; set; }
+        public string? BikeModel { get; set; }
+        public string? Location { get; set; }
+        public string? EmergencyContactPerson { get; set; }
+        public string? EmergencyContactNumber { get; set; }
+        public string? GTRNumber { get; set; }
+        public string? Status { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
