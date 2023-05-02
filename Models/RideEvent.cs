958e6540dbaf1dd87f93e05474b87a59384a84b3
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2023-05-02 21:33:06 +1000

    Add Ride Event and a Ride Event Category enum

---
+++ Models/RideEvent.cs
@@ -0,0 +1,16 @@
+﻿namespace Get_Together_Riders.Models
+{
+    public class RideEvent
+    {
+        public int RideEventID { get; set; }
+        public string EventName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public RideEventCategory EventCategory { get; set; } 
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        // how do i represent the list of users that are registered for this ride - could be confirmed or maybe (not exact terminology - refer DB group)
+        // we would evenutally sync a list of users from the Facebook group
+        // debating on if we just have two lists of riders? - ConfirmedRiders and TentativeRiders
+    }
+}
---
+++ Models/RideEventCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace Get_Together_Riders.Models
+{
+    public enum RideEventCategory
+    {
+        GTR8 = 800,
+        GTR10 = 1000,
+        GTR12 = 1200
+    }
+}
