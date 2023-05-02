5484d1790a096562f691ac107165d22c5e468490
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2023-05-02 22:02:57 +1000

    add comment with more details about rideevent users

--- Models/RideEvent.cs
+++ Models/RideEvent.cs
@@ -12,5 +12,6 @@
         // how do i represent the list of users that are registered for this ride - could be confirmed or maybe (not exact terminology - refer DB group)
         // we would evenutally sync a list of users from the Facebook group
         // debating on if we just have two lists of riders? - ConfirmedRiders and TentativeRiders
+        // Options, going, not going and interested
     }
 }
