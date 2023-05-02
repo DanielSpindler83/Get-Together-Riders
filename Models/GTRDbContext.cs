9c6a2452b661bf6ee76d1ffe9fe60e3de7c35206
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2023-05-02 21:09:26 +1000

    add GTR DbContext class shell

---
+++ Models/GTRDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Get_Together_Riders.Models
+{
+    public class GTRDbContext : DbContext
+    {
+
+        public GTRDbContext(DbContextOptions<GTRDbContext> options) : base(options)
+        { 
+        }
+
+        // add DbSets here....
+    }
+}
