8f3cb5001e8dc64232129d917f127d3ce3ebfb21
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2023-05-06 14:52:52 +1000

    Add a RideEventEnrollment class to serve as a junction table, between many to many relationship of Riders and RideEvents.

---
+++ Models/RideEventEnrollment.cs
@@ -0,0 +1,14 @@
+﻿using Get_Together_Riders.Models.Enums;
+
+namespace Get_Together_Riders.Models
+{
+    public class RideEventEnrollment
+    {
+        public int RideEventEnrollmentID { get; set; }
+        public int RiderID { get; set; }
+        public Rider Rider { get; set; }
+        public int RideEventID { get; set; }
+        public RideEvent RideEvent { get; set; }
+        public RiderState State { get; set; }
+    }
+}
