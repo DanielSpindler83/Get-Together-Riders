553c0dbb390afe94d4a89d3d901a3e72ced743af
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2023-05-12 20:45:49 +1000

    Run db migration to add identityuserId into Rider table

---
+++ Migrations/20230512104311_addRiderIdentityID.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Get_Together_Riders.Migrations
+{
+    public partial class addRiderIdentityID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IdentityUserId",
+                table: "Riders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdentityUserId",
+                table: "Riders");
+        }
+    }
+}
