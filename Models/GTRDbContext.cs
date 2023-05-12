b98e1c67e9454a7fc19f1de906d323ced09c6fcc
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2023-05-12 20:46:25 +1000

    add intereting comments about allowing nullable foreign key

--- Models/GTRDbContext.cs
+++ Models/GTRDbContext.cs
@@ -14,5 +14,17 @@ namespace Get_Together_Riders.Models
         public DbSet<Rider> Riders { get; set; }
         public DbSet<RideEvent> RideEvents { get; set; }
         public DbSet<RideEventEnrollment> RideEventEnrollments { get; set; }
+
+
+        //// allows nullable foreign key 
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Rider>()
+        //        .HasOne(r => r.IdentityUser)
+        //        .WithMany()
+        //        .HasForeignKey(r => r.IdentityUserId)
+        //        .OnDelete(DeleteBehavior.Restrict) // DeleteBehavior.Restrict is used to specify that when a related IdentityUser is deleted, the deletion is restricted and does not cascade to the Rider entity.
+        //        .HasConstraintName("FK_Riders_AspNetUsers_IdentityUserId");
+        //}
     }
 }
