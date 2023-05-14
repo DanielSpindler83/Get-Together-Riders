1b6973da8308e19bc8e96b0dc78095801002fd1f
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2023-05-14 15:34:21 +1000

    add ToString overload for rider

--- Models/Rider.cs
+++ Models/Rider.cs
@@ -20,5 +20,39 @@ namespace Get_Together_Riders.Models
         public string? Status { get; set; }
         public bool IsActive { get; set; } = true;
         public List<RideEventEnrollment> RideEventEnrollments { get; set; } = new List<RideEventEnrollment>();
+
+
+        public override string ToString()
+        {
+            return $"RiderID: {RiderID}\n" +
+                   $"IdentityUserId: {IdentityUserId ?? "null"}\n" +
+                   $"FirstName: {FirstName}\n" +
+                   $"LastName: {LastName}\n" +
+                   $"DisplayName: {DisplayName}\n" +
+                   $"Bio: {Bio ?? "null"}\n" +
+                   $"Email: {Email ?? "null"}\n" +
+                   $"PhoneNumber: {PhoneNumber ?? "null"}\n" +
+                   $"BikeModel: {BikeModel ?? "null"}\n" +
+                   $"Location: {Location ?? "null"}\n" +
+                   $"EmergencyContactPerson: {EmergencyContactPerson ?? "null"}\n" +
+                   $"EmergencyContactNumber: {EmergencyContactNumber ?? "null"}\n" +
+                   $"GTRNumber: {GTRNumber ?? "null"}\n" +
+                   $"Status: {Status ?? "null"}\n" +
+                   $"IsActive: {IsActive}\n" +
+                   $"RideEventEnrollments: {GetRideEventEnrollmentDetails()}";
+        }
+
+        private string GetRideEventEnrollmentDetails()
+        {
+            if (RideEventEnrollments.Count > 0)
+            {
+                return string.Join(", ", RideEventEnrollments);
+            }
+            else
+            {
+                return "No ride event enrollments";
+            }
+        }
     }
+
 }
