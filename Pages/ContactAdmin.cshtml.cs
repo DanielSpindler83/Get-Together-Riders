bdf89cc0d7863a8d3e78912be342e06a77570231
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2023-05-14 16:05:03 +1000

    Add new Contact Admin page and static page(not used) and redirect when rider not found in DB

---
+++ Pages/ContactAdmin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Get_Together_Riders.Pages
+{
+    public class ContactAdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
