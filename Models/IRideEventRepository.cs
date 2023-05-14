60e47ccd01008b4e10870645ab1b5b38c08973f6
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2023-05-14 16:16:57 +1000

    add Ride Event repo interface and class

---
+++ Models/IRideEventRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Get_Together_Riders.Models
+{
+    public interface IRideEventRepository
+    {
+        RideEvent GetRideEventById(int rideEventId);
+        void AddRideEvent(RideEvent rideEvent);
+        void UpdateRideEvent(RideEvent rideEvent);
+        void DeleteRideEvent(int rideEventId);
+        IEnumerable<RideEvent> GetAllRideEvents();
+    }
+}
