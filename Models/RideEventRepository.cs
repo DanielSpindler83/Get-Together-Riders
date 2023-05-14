0e4d696c6cd71bffea7e69d0a33f5ac5fc21a817
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2023-05-14 16:16:57 +1000

    add Ride Event repo interface and class

---
+++ Models/RideEventRepository.cs
@@ -0,0 +1,45 @@
+﻿namespace Get_Together_Riders.Models
+{
+    public class RideEventRepository : IRideEventRepository
+    {
+        private readonly GTRDbContext _gTRDbContext;
+
+        public RideEventRepository(GTRDbContext gTRDbContext)
+        {
+            _gTRDbContext = gTRDbContext;
+        }
+
+        public RideEvent GetRideEventById(int rideEventId)
+        {
+            return _gTRDbContext.RideEvents.Find(rideEventId);
+        }
+
+        public void AddRideEvent(RideEvent rideEvent)
+        {
+            _gTRDbContext.RideEvents.Add(rideEvent);
+            _gTRDbContext.SaveChanges();
+        }
+
+        public void UpdateRideEvent(RideEvent rideEvent)
+        {
+            _gTRDbContext.RideEvents.Update(rideEvent);
+            _gTRDbContext.SaveChanges();
+        }
+
+        public void DeleteRideEvent(int rideEventId)
+        {
+            var rideEvent = _gTRDbContext.RideEvents.Find(rideEventId);
+            if (rideEvent != null)
+            {
+                _gTRDbContext.RideEvents.Remove(rideEvent);
+                _gTRDbContext.SaveChanges();
+            }
+        }
+
+        public IEnumerable<RideEvent> GetAllRideEvents()
+        {
+            return _gTRDbContext.RideEvents.ToList();
+        }
+    }
+
+}
