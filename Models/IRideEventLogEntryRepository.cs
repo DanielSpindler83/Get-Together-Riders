5f4d432b832d1fa57bc5f3df3572f9c50c1101c4
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2023-05-15 19:45:29 +1000

    Add RideEventLogEntry repo

---
+++ Models/IRideEventLogEntryRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Get_Together_Riders.Models
+{
+    public interface IRideEventLogEntryRepository
+    {
+        RideEventLogEntry GetRideEventLogEntryById(int id);
+        void AddRideEventLogEntry(RideEventLogEntry rideEventLogEntry);
+        void UpdateRideEventLogEntry(RideEventLogEntry rideEventLogEntry);
+        void DeleteRideEventLogEntry(int id);
+        void SaveChanges();
+    }
+}
