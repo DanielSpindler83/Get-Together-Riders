81e2cffa2b1088fac45dce921bc43fab518b3ef6
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2023-05-15 19:45:29 +1000

    Add RideEventLogEntry repo

---
+++ Models/RideEventLogEntryRepository.cs
@@ -0,0 +1,48 @@
+﻿namespace Get_Together_Riders.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    using System.Linq;
+
+    using System.Linq;
+
+    public class RideEventLogEntryRepository : IRideEventLogEntryRepository
+    {
+        private readonly GTRDbContext _gTRDbContext;
+
+        public RideEventLogEntryRepository(GTRDbContext gTRDbContext)
+        {
+            _gTRDbContext = gTRDbContext;
+        }
+
+        public RideEventLogEntry GetRideEventLogEntryById(int id)
+        {
+            return _gTRDbContext.RideEventLogEntries.FirstOrDefault(r => r.RideEventLogEntryID == id);
+        }
+
+        public void AddRideEventLogEntry(RideEventLogEntry rideEventLogEntry)
+        {
+            _gTRDbContext.RideEventLogEntries.Add(rideEventLogEntry);
+        }
+
+        public void UpdateRideEventLogEntry(RideEventLogEntry rideEventLogEntry)
+        {
+            _gTRDbContext.RideEventLogEntries.Update(rideEventLogEntry);
+        }
+
+        public void DeleteRideEventLogEntry(int id)
+        {
+            var rideEventLogEntry = GetRideEventLogEntryById(id);
+            if (rideEventLogEntry != null)
+            {
+                _gTRDbContext.RideEventLogEntries.Remove(rideEventLogEntry);
+            }
+        }
+
+        public void SaveChanges()
+        {
+            _gTRDbContext.SaveChanges();
+        }
+    }
+
+
+}
