5849ba00011b4dc011a834c33b295fefe6411b69
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2023-05-16 15:07:52 +1000

    Submit new Event Log Entry form working - NO Validation

--- Pages/AddRideEventLogEntry.cshtml.cs
+++ Pages/AddRideEventLogEntry.cshtml.cs
@@ -8,29 +8,32 @@ namespace Get_Together_Riders.Pages
     public class AddRideEventLogEntryModel : PageModel
     {
         private readonly IRideEventLogEntryRepository _rideEventLogEntryRepository;
+        private readonly IRideEventRepository _rideEventRepository;
         private readonly IRiderRepository _riderRepository; 
 
         [BindProperty]
         public RideEventLogEntry RideEventLogEntry { get; set; }
 
-        public int RiderID { get; set; }
         public int RideEventID { get; set; }
+        public Rider CurrentRider { get; private set; }
 
-        public AddRideEventLogEntryModel(IRideEventLogEntryRepository rideEventLogEntryRepository, IRiderRepository riderRepository)
+
+        public AddRideEventLogEntryModel(IRideEventLogEntryRepository rideEventLogEntryRepository, IRiderRepository riderRepository, IRideEventRepository rideEventRepository)
         {
             _rideEventLogEntryRepository = rideEventLogEntryRepository;
             _riderRepository = riderRepository;
+            _rideEventRepository = rideEventRepository;
         }
 
         public IActionResult OnGet(int rideEventId)
         {
-            RideEventID = rideEventId; 
+            RideEventID = rideEventId;
 
             // Get the identity ID of the logged-in user
             var identityUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
             // Use the identity ID to fetch the Rider details
-            var rider = _riderRepository.GetRiderByIdentityId(identityUserId);
+            CurrentRider = _riderRepository.GetRiderByIdentityId(identityUserId);
 
             // setup the form ready for user to complete it
             // what do we need to show? Mirror from
@@ -40,15 +43,29 @@ namespace Get_Together_Riders.Pages
 
         public IActionResult OnPost()
         {
+
+            // Get the identity ID of the logged-in user
+            var identityUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            // Use the identity ID to fetch the Rider details
+            CurrentRider = _riderRepository.GetRiderByIdentityId(identityUserId);
+
+            // another approach here is to use a ViewModel class just for the form fields
+            ModelState.Remove("RideEventLogEntry.Rider");
+            ModelState.Remove("RideEventLogEntry.RideEvent");
+
             if (!ModelState.IsValid)
             {
                 return Page();
             }
 
+            RideEventLogEntry.Rider = CurrentRider;
+            RideEventLogEntry.RideEvent = _rideEventRepository.GetRideEventById(RideEventLogEntry.RideEventID);
+
             _rideEventLogEntryRepository.AddRideEventLogEntry(RideEventLogEntry);
             _rideEventLogEntryRepository.SaveChanges();
 
-            return RedirectToPage("/RideEvents/Index");
+            return RedirectToPage("/RideEvents");
         }
     }
 }
