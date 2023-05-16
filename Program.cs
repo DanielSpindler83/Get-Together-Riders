da248eb95286cb8d4a0b523dc400bb92c4e072db
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2023-05-16 20:36:21 +1000

    Setup Admin Role and test it

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace Get_Together_Riders
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -71,6 +71,7 @@ namespace Get_Together_Riders
             
             // Order of this is IMPORTANT - add auth before adding the DefaultIdentity
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddRoles<IdentityRole>() // https://www.youtube.com/watch?v=Y6DCP-yH-9Q - setting up roles
                 .AddEntityFrameworkStores<GTRDbContext>();
 
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -100,6 +101,36 @@ namespace Get_Together_Riders
 
             // run our DB seed - will only populate data if none exists in the DB
             DbInitializer.Seed(app);
+
+
+            // https://www.youtube.com/watch?v=Y6DCP-yH-9Q - setting up roles
+            using (var scope = app.Services.CreateScope())
+            {
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                var roles = new[] { "Admin", "Rider" };
+                foreach (var role in roles)
+                {
+                    if (!await roleManager.RoleExistsAsync(role))
+                        await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+
+            // assign the new admin role to ruckus_gypsy@hotmail.com
+            using (var scope = app.Services.CreateScope())
+            {
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+                string email = "ruckus_gypsy@hotmail.com"; 
+
+                if(await userManager.FindByEmailAsync(email) != null)
+                {
+                    var user = await userManager.FindByEmailAsync(email);
+                    await userManager.AddToRoleAsync(user, "Admin");
+                }
+            }
+
+            // [Authorize(Roles = "Admin")] - block we use for restricting to a role
+
+
 
             app.Run();
         }
