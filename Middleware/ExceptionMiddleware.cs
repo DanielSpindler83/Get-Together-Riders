b6f2f832feb4bd9c5a4bad38f2e4212f05f8554d
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2023-05-29 21:54:49 +1000

    add global exception middleware (basic)

---
+++ Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,40 @@
+﻿namespace Get_Together_Riders.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                Console.WriteLine("Error: " + error.Message);
+                Console.WriteLine("Error Details: " + context.Request.Path);
+
+                if (error.InnerException != null)
+                    Console.WriteLine("Inner Error: " + error.InnerException.Message);
+
+                // Handle Exceptions
+
+                // Return StatusCode, Message, Details or TraceId etc as restult
+
+                //Return StatusCode, Message, Details as result
+                await response.WriteAsync("Error");
+            }
+        }
+    }
+}
