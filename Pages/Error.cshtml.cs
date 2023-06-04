53e70536b1471c36b7e13bf6e9e3e9a67db9ff93
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2023-06-04 14:32:58 +1000

    Add more to ErrorHandler UI page

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
@@ -6,9 +7,13 @@ namespace Get_Together_Riders.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
+    [AllowAnonymous]
     public class ErrorModel : PageModel
     {
         public string? RequestId { get; set; }
+
+        public Activity? CurrentActivity { get; set; }
+        public string? TraceId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
@@ -22,6 +27,11 @@ namespace Get_Together_Riders.Pages
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            CurrentActivity = Activity.Current;
+            TraceId = HttpContext.TraceIdentifier;
+
+            var userName = User.Identity?.IsAuthenticated ?? false ? User.Identity.Name : "";
+            _logger.LogWarning("User {userName} experienced an error.", userName);
         }
     }
 }
